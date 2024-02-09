8ce666ef83e59ed3c360498661390a0f1b8e1963
bcrusinski <159018893+bcrusinski@users.noreply.github.com>
2024-02-09 18:35:20 -0300

    Elemento adicional ao fundo
    Adição da imagem da alga e emplemetação de três delas ao fundo

--- peixe.js
+++ peixe.js
@@ -15,16 +15,23 @@ var game = new Phaser.Game(config);
 var peixinho;
 
 function preload(){
+    //Carrega as três imagens que serão utilizadas
     this.load.image("mar", "assets/bg_azul-claro.png");
     this.load.image("logo", "assets/logo-inteli_azul.png");
     this.load.image("peixe", "assets/peixes/peixe_turquesa.png");
+    this.load.image("alga", "assets/alga.png");
 }
 
 function create(){
     this.add.image(400, 300, "mar");
     this.add.image(400, 525, "logo").setScale(0.8);
-    peixinho = this.add.image(400, 300, "peixe")
+    peixinho = this.add.image(400, 300, "peixe");
 
+    //Adiciona três algas ao cenário (tentei implementar como função, mas, devido a problemas tecnicos e inexperiencia com o framework, desisti por hora)
+    this.add.image(Phaser.Math.Between(100, 700), Phaser.Math.Between(100, 500), "alga").setScale(0.11);
+    this.add.image(Phaser.Math.Between(100, 700), Phaser.Math.Between(100, 500), "alga").setScale(0.1);
+    this.add.image(Phaser.Math.Between(100, 700), Phaser.Math.Between(100, 500), "alga").setScale(0.08);
+    
     peixinho.setOrigin(0.5,0.5).setFlip(true,false);
 
 }
